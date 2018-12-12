10aefdf023990a8c0ffed35aa61e534879eb7bd2
g-t-okuniewicz <ted-sites@live.com>
2018-12-12 14:46:10 +0000

    reading from a text file

--- web.js
+++ web.js
@@ -1,16 +1,31 @@
 var http = require("http");
+var fs = require("fs");
 
-function process_request(req, res) {
-    var body = 'Thanks for calling!\n'
-    var content_length = body.length;
-    
-    res.writeHead(200, {
-        'Content-Length': content_length,
-        'Content-Type': 'text/plain'
+function handle_incoming_request(req, res) {
+    console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+    load_sensor_data(function(err, readings){
+        if(err) {
+            console.log("Could not read file");
+        }
+        console.log(readings);
+        res.writeHead(200, {"Content-Type":"application/json"});
+        res.end(JSON.stringify({temperature:readings}));
     });
-    
-    res.end(body);
 }
 
-var s = http.createServer(process_request);
+function load_sensor_data(callback) {
+    fs.readFile(
+        "sensorlog.txt",'utf8',
+        function(err, readings) {
+            if(err) {
+                callback(err);
+                return;
+            }
+            callback(null, readings);
+        }
+    );
+}
+
+var s = http.createServer(handle_incoming_request);
+
 s.listen(8080);
