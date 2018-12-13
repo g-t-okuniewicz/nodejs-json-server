601fec9dc7d12906c103fe48c48b0059a179edef
g-t-okuniewicz <ted-sites@live.com>
2018-12-13 00:42:35 +0000

    simple JSON server

--- web.js
+++ web.js
@@ -1,29 +1,26 @@
-var http = require("http");
-var fs = require("fs");
+const http = require("http");
+const filePath = 'sensorlog.txt';
+const csvtojson = require("csvtojson");
 
 function handle_incoming_request(req, res) {
     console.log("INCOMING REQUEST: " + req.method + " " + req.url);
-    load_sensor_data(function(err, readings){
-        if(err) {
-            console.log("Could not read file");
-        }
-        console.log(readings);
-        res.writeHead(200, {"Content-Type":"application/json"});
-        res.end(JSON.stringify({temperature:readings}));
-    });
-}
 
-function load_sensor_data(callback) {
-    fs.readFile(
-        "sensorlog.txt",'utf8',
-        function(err, readings) {
-            if(err) {
-                callback(err);
-                return;
-            }
-            callback(null, readings);
-        }
-    );
+    csvtojson().on('header', function(header) {
+        console.log("Header: " + header);
+    })
+
+    csvtojson().on('error', function(err) {
+        console.log("There was an error: " + err);
+    });
+
+    csvtojson().on('done', function(error) {
+        console.log("Done with error: " + error);
+    })
+
+    csvtojson().fromFile(filePath).then(function(jsonObj) {
+        res.writeHead(200, {"Content-Type":"application/json"});
+        res.end(JSON.stringify(jsonObj));
+    });
 }
 
 var s = http.createServer(handle_incoming_request);
