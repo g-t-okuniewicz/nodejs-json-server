7dda38c255ce592c83b2c6a9674d7b85ac72460c
g-t-okuniewicz <ted-sites@live.com>
2018-12-13 00:46:28 +0000

    cleaned up

--- web.js
+++ web.js
@@ -5,18 +5,6 @@ const csvtojson = require("csvtojson");
 function handle_incoming_request(req, res) {
     console.log("INCOMING REQUEST: " + req.method + " " + req.url);
 
-    csvtojson().on('header', function(header) {
-        console.log("Header: " + header);
-    })
-
-    csvtojson().on('error', function(err) {
-        console.log("There was an error: " + err);
-    });
-
-    csvtojson().on('done', function(error) {
-        console.log("Done with error: " + error);
-    })
-
     csvtojson().fromFile(filePath).then(function(jsonObj) {
         res.writeHead(200, {"Content-Type":"application/json"});
         res.end(JSON.stringify(jsonObj));
